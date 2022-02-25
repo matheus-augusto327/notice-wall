c53a39bf3352e28d873de2ac25da6ba4e01d4208
Matheus <matheus.augustoreal@gmail.com>
2022-02-25 20:01:24 -0300

    Create router

--- index.js
+++ index.js
@@ -1,21 +1,11 @@
 const express = require("express")
 const app = express()
-const posts = require("./model/posts")
+const path = require("path")
+const apiRoute = require("./routes/api")
 const PORT = 3000
 
-app.get("/all", (req, res) => {
-    res.json(JSON.stringify(posts.getAll()))
-})
-
-app.post("/new", express.json(), (req, res) => {
-    let title = req.body.title
-    let description = req.body.description
-
-    posts.newPost(title, description)
-
-    res.send("Post adicionado")
-
-})
+app.use("/api", apiRoute)
+app.use("/", express.static(path.join(__dirname, "public")))
 
 app.listen(PORT, () => {
     console.log("Server running on port", PORT)
